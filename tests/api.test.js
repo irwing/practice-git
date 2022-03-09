0c01cc0c520dc941237c4358637e10b2543928a5
Irwing Naranjo <khalisser@gmail.com>
2022-03-08 23:04:56 -0300

    correction tests

--- tests/api.test.js
+++ tests/api.test.js
@@ -1,4 +1,7 @@
 const axios = require("axios");
+const app = require("../index.js");
+const request = require("supertest");
+
 const env = require("dotenv");
 
 env.config();
@@ -10,8 +13,7 @@ const urlSwapi = process.env.URL_SWAPI;
 describe("swapi", () => {
   // test api https://swapi.dev/api/people/1/
   it("Api swapi.dev should return a 200 response", async () => {
-    const response = await axios.get(`${urlSwapi}/people/1/`);
-    // const response = await axios.get("https://swapi.dev/api/people/1/");
+    const response = await axios.get(`${urlSwapi}/people/1`);
     expect(response.status).toBe(200);
     expect(response.data.name).toBe("Luke Skywalker");
   });
@@ -19,38 +21,37 @@ describe("swapi", () => {
 
 describe("api", () => {
   // test api http://localhost:3000/api
-  it("get person should return a 200 response", async () => {
-    const response = await axios.get(`${urlApi}:${portApi}/api/person/1`);
+  it("should return a 200 response when api is active", async () => {
+    const response = await axios.get(`${urlApi}:${portApi}/api`);
     expect(response.status).toBe(200);
     expect(response.data).toContain("Api Star Wars");
   });
 
   // test api http://localhost:3000/api/person/1
-  it("get person should return a 200 response", async () => {
-    const response = await axios.get(`${urlApi}:${portApi}/api/person/1`);
+  it("should return a 200 response when find a person", async () => {
+    const response = await axios.get(`${urlApi}:${portApi}/api/persons/1`);
     expect(response.status).toBe(200);
-    expect(response.data.name).toContain("Luke Skywalker");
+    expect(response.data.name).toBe("Luke Skywalker");
   });
 
   // test api http://localhost:3000/api/docs
-  it("get person should return a 200 response", async () => {
+  it("should return a 200 response if the documentatin is active", async () => {
     const response = await axios.get(`${urlApi}:${portApi}/api/docs`);
     expect(response.status).toBe(200);
   });
 
   // test api http://localhost:3000/api/person/inavalidtext
-  it("any route not defined should return a 200 response", async () => {
+  it("should 404 if person no is found", async () => {
     const search = Math.random().toString(36).substring(2, 15);
-
-    const response = await axios.get(
-      `${urlApi}:${portApi}/api/person/${search}`
-    );
+    const response = await request(app).get(`/api/persons/${search}`).send();
     expect(response.status).toBe(404);
+    expect(response.body.error).toBe("Person not found");
   });
 
   // test api http://localhost:3000/api/inavalidendpoint
-  it("any route not defined should return a 200 response", async () => {
-    const response = await axios.get(`${urlApi}:${portApi}/api/hello/world`);
+  it("should 404 if no route is found", async () => {
+    const response = await request(app).get(`/api/inavalidendpoint`).send();
     expect(response.status).toBe(404);
+    expect(response.body.error).toBe("Route not found");
   });
 });
