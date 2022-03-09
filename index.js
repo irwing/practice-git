9ec4ad83dc592ee8a8ba6095cfb58652e46bcd4d
Irwing Naranjo <khalisser@gmail.com>
2022-03-08 21:09:25 -0300

    server init ready

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const env = require("dotenv");
+
+env.config();
+
+const optionsCors = { origin: "*" };
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors(optionsCors));
+app.use(express.json());
+
+// routes
+app.get("/", (req, res) => res.send("Api is running"));
+
+// listen
+app.listen(port, () =>
+  console.log(`server listening on port ${process.env.PORT}!`)
+);
+
+// export
+module.exports = app;
