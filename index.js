6ba5e37319b9849627cf993ba4724502fef71b28
Irwing Naranjo <khalisser@gmail.com>
2022-03-08 23:05:33 -0300

    add:feacture getPerson

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const env = require("dotenv");
+const axios = require("axios");
+
+// const personsController = require("./src/controllers/persons");
 
 env.config();
 
@@ -13,10 +16,30 @@ app.use(cors(optionsCors));
 app.use(express.json());
 
 // routes
-app.get("/", (req, res) => res.send("Api is running"));
+app.get("/api", (req, res) => res.send("Api Star Wars"));
+app.get("/api/docs", (req, res) => res.send("Docs Star Wars"));
+// app.use("api/persons/:id", personsController);
+// route with parameters
+app.get("/api/persons/:id", async (req, res) => {
+  const personId = req.params.id;
+
+  try {
+    const response = await axios.get(
+      `${process.env.URL_SWAPI}/people/${personId}/`
+    );
+    res.json(response.data);
+  } catch (error) {
+    res.status(404).send({ error: "Person not found" });
+  }
+});
+
+// return 404 if no route is found
+app.use((req, res) => {
+  res.status(404).send({ error: "Route not found" });
+});
 
 // listen
-app.listen(port, () =>
+app.listen(process.env.PORT_API, () =>
   console.log(`server listening on port ${process.env.PORT_API}!`)
 );
 
